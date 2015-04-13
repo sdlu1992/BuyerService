80fc872f77257a31ccef3159848937b4dc57a787
sdlu1992 <sdlu1992@gmail.com>
2015-04-13 11:49:40 +0800

    fix bugs:  get_goods_by_catgory

--- main/views.py
+++ main/views.py
@@ -204,9 +204,10 @@ def get_goods_by_category(request):
     error_message = ''
     print(request.method)
     if request.method == 'POST':
-        req = json.loads(request.body)
-        print req
-        cate = req['category']
+        # req = json.loads(request.body)
+        # print req
+        # cate = req['category']
+        cate = request.POST.get('category')
         goods = Goods.objects.filter(category=cate)
         response['len'] = len(goods)
         if len(goods) != 0:
