c3efec2880698327cc169306316542b487620fb9
sdlu1992 <sdlu1992@gmail.com>
2015-04-17 20:39:53 +0800

    fix bugs addOrder

--- main/views.py
+++ main/views.py
@@ -369,15 +369,15 @@ def add_order(request):
         else:
             buyer = Buyer.objects.filter(token_web=request.session.get('token', ''))
     elif request.method == 'POST':
-        req = json.loads(request.body)
-        print req
-        r_platform = req['platform']
-        r_goods = json.loads(req['goods'])
-        buyer = Buyer.objects.filter(token=req['token'])
-        # r_platform = request.POST.get('platform')
-        # r_goods = json.loads(request.POST.get('test'))
-        # print r_goods
-        # buyer = Buyer.objects.filter(token_web=request.session.get('token'))
+        # req = json.loads(request.body)
+        # print req
+        # r_platform = req['platform']
+        # r_goods = json.loads(req['goods'])
+        # buyer = Buyer.objects.filter(token=req['token'])
+        r_platform = request.POST.get('platform')
+        r_goods = json.loads(request.POST.get('test'))
+        print r_goods
+        buyer = Buyer.objects.filter(token_web=request.session.get('token'))
         for foo in r_goods:
             good = Goods.objects.get(id=foo['id'])
             goods.append(goods)
