58c34b4e7a7c9dbc378abf2c639215370ac3b936
sdlu1992 <sdlu1992@gmail.com>
2015-04-17 19:15:53 +0800

    fix bugs login

--- main/views.py
+++ main/views.py
@@ -208,9 +208,10 @@ def get_goods_by_category(request):
     error_message = ''
     print(request.method)
     if request.method == 'POST':
-        req = json.loads(request.body)
-        print req
-        cate = req['category']
+        # req = json.loads(request.body)
+        # print req
+        # cate = req['category']
+        cate = request.POST.get('test')
         goods = Goods.objects.filter(category=cate)
         response['len'] = len(goods)
         if len(goods) != 0:
@@ -403,7 +404,7 @@ def test(request):
     if request.method == 'GET':
         return render_to_response('test.html')
     elif request.method == 'POST':
-        return add_order(request)
+        return get_goods_by_category(request)
 
 
 def get_buyer_by_phone(phone_number):
